2408feb74081e43eea0515227d5385e38035f302
x0f5c3 <x0f5c3@tutanota.com>
2023-11-05 17:42:21 +0100

    Add wails and tauri

---
+++ frontend.go
@@ -0,0 +1,6 @@
+package open_resume
+
+import "embed"
+
+//go:embed all:out
+var Assets embed.FS
--- next.config.js
+++ next.config.js
@@ -5,7 +5,12 @@ const nextConfig = {
   // building the app. Since pdfjs-dist is only used on client side, we disable
   // the canvas package for webpack
   // https://github.com/mozilla/pdf.js/issues/16214
-  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
   webpack: (config) => {
     // Setting resolve.alias to false tells webpack to ignore a module
     // https://webpack.js.org/configuration/resolve/#resolvealias
